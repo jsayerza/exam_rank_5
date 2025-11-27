9562691b80532f1632673ee8267537342d992521
Jordi Sayerza <jsayerza.comptes@gmail.com>
2025-11-27 14:06:35 +0100

    polyset v1

---
+++ polyset.v1/searchable_array_bag.cpp
@@ -0,0 +1,10 @@
+#include "searchable_array_bag.hpp"
+
+searchable_array_bag::searchable_array_bag(){}
+searchable_array_bag::searchable_array_bag(const searchable_array_bag& other){}
+searchable_array_bag::~searchable_array_bag(){}
+
+searchable_array_bag& searchable_array_bag::operator=(const searchable_array_bag& other){}
+
+bool searchable_array_bag::has(int) const
+{}
---
+++ polyset.v1/searchable_array_bag.hpp
@@ -0,0 +1,19 @@
+#ifndef SEARCHEABLE_ARRAY_BAG_HPP
+# define SEARCHEABLE_ARRAY_BAG_HPP
+
+# include "./subject/array_bag.hpp"
+# include "./subject/searchable_bag.hpp"
+
+class searchable_array_bag : public array_bag, public searchable_bag
+{
+	searchable_array_bag();
+	searchable_array_bag(const searchable_array_bag& other);
+	~searchable_array_bag();
+
+	searchable_array_bag& operator=(const searchable_array_bag& other);
+
+	bool has(int) const;
+
+};
+
+#endif
